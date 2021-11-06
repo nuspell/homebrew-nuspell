b5a47c46583eeb4671a38fbf09ced0e9ab802505
Dimitrij Mijoski <dmjpp@hotmail.com>
2021-11-06 17:55:15 +0100

    Keep it simple

--- Formula/nuspell.rb
+++ Formula/nuspell.rb
@@ -7,19 +7,6 @@ class Nuspell < Formula
   depends_on "icu4c"
   depends_on "cmake" => :build
   depends_on "pandoc" => :build
-  depends_on "gnu-tar" => :test
-  depends_on "grep" => :test
-  uses_from_macos "binutils" => :test
-
-  resource "test_dictionary" do
-    url "http.us.debian.org/debian/pool/main/s/scowl/hunspell-en-us_2019.10.06-1_all.deb"
-    sha256 "b15bb8a8a891fa4efb3705e9579f300bf6ab1929a335b2090e053d1b02fbb3e8"
-  end
-
-  resource "test_wordlist" do
-    url "http.us.debian.org/debian/pool/main/s/scowl/wamerican-small_2019.10.06-1_all.deb"
-    sha256 "0dae43bfdc5fc3cf9560509f927a7cc959616d86d121c032616c37cdf58d5b99"
-  end
 
   def install
     mkdir "build" do
@@ -39,12 +26,6 @@ class Nuspell < Formula
   end
 
   test do
-    testpath.install resource("test_dictionary")
-    system "ar", "x", "hunspell-en-us_2019.10.06-1_all.deb"
-    system "tar", "xf", "data.tar.xz"
-    testpath.install resource("test_wordlist")
-    system "ar", "x", "wamerican-small_2019.10.06-1_all.deb"
-    system "tar", "xf", "data.tar.xz"
-    assert(pipe_output("#{bin}/nuspell -d usr/share/hunspell/en_US usr/share/dict/american-english-small | grep '^*'").size > 45000)
+    system "#{bin}/nuspell", "--help"
   end
 end
